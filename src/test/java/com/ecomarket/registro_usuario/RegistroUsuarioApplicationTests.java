215f4f81c2a10f21d45871fff73a5f5fa366d36d
Jona <jon.vidals@duocuc.cl>
2025-05-10 12:53:34 -0400

    Microservicio registro de usuarios

---
+++ src/test/java/com/ecomarket/registro_usuario/RegistroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarket.registro_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
